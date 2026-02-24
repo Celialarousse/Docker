290bd523765128e87c9ce8abdae17481d6950f75
Larousse <celia.larousse@hotmail.com>
2026-02-24 21:06:38 +0100

    modification config.php

--- src/Formulaire/config.php
+++ src/Formulaire/config.php
@@ -7,9 +7,9 @@ $charset = 'utf8mb4'; // Encodage des caractères
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // Active les exceptions en cas d’erreur
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,      // Retourne les résultats sous forme de tableau associatif
+    PDO::ATTR_EMULATE_PREPARES   => false,                  // Désactive l’émulation des requêtes préparées (plus sécurisé)
 ];
 
 try {
